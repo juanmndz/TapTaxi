0f85a5f1ac209c2683b0e2e155cb2d5a956b8270
juanmndz <juanmndz19@gmail.com>
2016-02-14 15:50:30 -0700

    Full video of Part 1 Done
    U w0t m8

---
+++ public_html/static/js/main.js
@@ -0,0 +1,33 @@
+var GAME_WIDTH = 480;
+var GAME_HEIGHT = 640;
+
+var state = {
+	init: init,
+	preload: preload,
+	update: update,
+	create: create
+
+};
+
+var phaserGame = new Phaser.Game(
+	GAME_WIDTH,
+	GAME_HEIGHT,
+	Phaser.AUTO, // Auto will switch betweem WebGL and Canvas
+	'container',
+	state
+	);
+
+var taxiGame = new TTTGame(phaserGame);
+
+function init () {
+	taxiGame.init();
+}
+function create () {
+	taxiGame.create();
+}
+function preload () {
+	taxiGame.preload();
+}
+function update () {
+	taxiGame.update();
+}
