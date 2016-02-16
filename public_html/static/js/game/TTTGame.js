d877c9c902eb1504aeabe1a7633094b966d4f72c
juanmndz <juanmndz19@gmail.com>
2016-02-15 17:35:31 -0700

    Move Road Tiles

--- public_html/static/js/game/TTTGame.js
+++ public_html/static/js/game/TTTGame.js
@@ -1,11 +1,32 @@
 var TTTGame = (function(){
 
+	var ANGLE = 26.55;
+
 	function TTTGame(phaserGame) {
 		this.game = phaserGame;
+		this.arrTiles = [];
 	}
 
+	TTTGame.prototype.generateRoad = function() {
+		var sprite = this.game.add.sprite(0, 0, 'tile_road_1');
+		sprite.anchor.setTo(0.5, 0.5);
+		sprite.x = this.game.world.centerX;
+		sprite.y = this.game.world.centerY;
+		this.arrTiles.push(sprite);
+	}
+
+	TTTGame.prototype.moveTiles = function() {
+		var i = this.arrTiles.length - 1;
+		while (i >= 0) {
+			var sprite = this.arrTiles[i];
+			sprite.x -= Math.cos( ANGLE * Math.PI / 180);
+			sprite.y += Math.sin( ANGLE * Math.PI / 180);
+			i--;
+		}
+	};
+
 	TTTGame.prototype.init = function() {
-		this.game.stage.backgroundColor = '#9bd3e1'
+		this.game.stage.backgroundColor = '#9bd3e1';
 		this.game.add.plugin(Phaser.Plugin.Debug);
 
 	};
@@ -17,14 +38,11 @@ var TTTGame = (function(){
 	};
 
 	TTTGame.prototype.create = function() {
-		var sprite = this.game.add.sprite(0, 0, 'tile_road_1');
-		sprite.anchor.setTo(0.5, 0.5);
-		sprite.x = this.game.world.centerX;
-		sprite.y = this.game.world.centerY;
-
+		this.generateRoad();
 	};
-
-	TTTGame.prototype.update = function() {
+	 
+	 TTTGame.prototype.update = function() {
+	 	this.moveTiles();
 
 	};
 
