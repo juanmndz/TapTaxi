1f5c1fffdf603f39225bae7716111610b42e109e
juanmndz <juanmndz19@gmail.com>
2016-02-16 15:30:52 -0700

    Add Scores

--- public_html/static/js/main.js
+++ public_html/static/js/main.js
@@ -2,32 +2,31 @@ var GAME_WIDTH = 480;
 var GAME_HEIGHT = 640;
 
 var state = {
-	init: init,
 	preload: preload,
+	init: init,
 	update: update,
 	create: create
-
 };
 
 var phaserGame = new Phaser.Game(
 	GAME_WIDTH,
 	GAME_HEIGHT,
-	Phaser.AUTO, // Auto will switch betweem WebGL and Canvas
-	'container',
-	state
-	);
+	Phaser.AUTO, // Renderer (Canvas or WebGL)
+	'container', // ID for the containing tag
+	state // State object
+);
 
 var taxiGame = new TTTGame(phaserGame);
 
-function init () {
-	taxiGame.init();
-}
-function create () {
-	taxiGame.create();
-}
-function preload () {
+function preload() {
 	taxiGame.preload();
 }
-function update () {
+function init() {
+	taxiGame.init();
+}
+function create() {
+	taxiGame.create();
+}
+function update() {
 	taxiGame.update();
 }
